fc8372c724f98daa305343869b98a71a673b1929
Eugenio Miró <eugenio.miro@gmail.com>
2019-10-25 22:24:21 -0300

    net core 1.1 vs2017

--- MvcHist/Program.cs
+++ MvcHist/Program.cs
@@ -16,6 +16,7 @@ namespace MvcHist
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
 
             host.Run();
